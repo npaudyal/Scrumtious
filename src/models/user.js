4d26d2775c7f82d2d290f1773fb914b7d054c577
Susan Subedi <susansubedi34@gmail.com>
2020-02-14 17:18:04 -0600

    Fixed issue with use rmodel

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const Task = require("./task");
 
 const userSchema = new mongoose.Schema(
   {
@@ -111,7 +110,6 @@ userSchema.pre("save", async function(next) {
 // Delete user tasks when user is removed
 userSchema.pre("remove", async function(next) {
   const user = this;
-  await Task.deleteMany({ owner: user._id });
   next();
 });
 
