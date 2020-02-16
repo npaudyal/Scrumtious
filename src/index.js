3dea9200c474cc42ab75dc08ab785de2ffc7c1a8
Susan Subedi <ssubedi1@go.olemiss.edu>
2020-02-15 23:04:44 -0600

    Fixed all the issue with routing

--- src/index.js
+++ src/index.js
@@ -6,31 +6,11 @@ const recipeRouter = require("./routers/recipe");
 
 const app = express();
 const port = process.env.PORT;
-const fs = require("fs");
-const path = require("path");
+
 app.use(express.json());
-app.use(userRouter);
-app.use(recipeRouter);
-
-// const description =
-//   "<ol><li>Place first five items into a blender and pulse a few times. Add the next berry and pulse 2-3 times. Repeat until all berries have been added, then blend on high until smooth.</li></ol>";
-// let mama = description;
-
-// if (description.includes("<ol>")) {
-//   mama = mama.replace("<ol>", "");
-// }
-// if (description.includes("<li>")) {
-//   mama = mama.replace("<li>", "");
-// }
-// if (description.includes("</li>")) {
-//   mama = mama.replace("</ol>", "");
-// }
-// if (description.includes("</ol>")) {
-//   mama = mama.replace("</li>", "");
-// }
-// console.log(mama);
+app.use("users", userRouter);
+app.use("recipes", recipeRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,91 +1,93 @@
 const express = require("express");
-const multer = require('multer')
-const sharp = require('sharp')
-const User = require('../models/user')
-const auth = require('../middleware/auth')
-const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
+const multer = require("multer");
+const sharp = require("sharp");
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
 
 const router = new express.Router();
 
+router.post("/", async (req, res) => {
+  const user = new User(req.body);
 
+  try {
+    await user.save();
+    sendWelcomeEmail(user.email, user.name);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-router.post('/users', async (req, res) => {
-    const user = new User(req.body)
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
 
-    try {
-        await user.save()
-        sendWelcomeEmail(user.email, user.name)
-         const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+router.post("/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
 
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({ user, token })
-    } catch (e) {
-        res.status(400).send()
-    }
-})
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.post('/users/logout', auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token
-        })
-        await req.user.save()
+router.post("/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.get("/me", auth, async (req, res) => {
+  res.send(req.user);
+});
 
-router.post('/users/logoutAll', auth, async (req, res) => {
-    try {
-        req.user.tokens = []
-        await req.user.save()
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+router.patch("/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "recipe"];
+  const isValidOperation = updates.every(update =>
+    allowedUpdates.includes(update)
+  );
 
-router.get('/users/me', auth, async (req, res) => {
-    res.send(req.user)
-})
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
 
-router.patch('/users/me', auth, async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'recipe']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+  try {
+    updates.forEach(update => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-        res.send(req.user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-router.delete('/users/me', auth, async (req, res) => {
-    try {
-        await req.user.remove()
-        sendCancelationEmail(req.user.email, req.user.name)
-        res.send(req.user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
+router.delete("/me", auth, async (req, res) => {
+  try {
+    await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
 module.exports = router;
